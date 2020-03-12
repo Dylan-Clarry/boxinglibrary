10709f364e9f615a06c9dd7de5f07fd84d11b527
myku5980 <40702716+myku5980@users.noreply.github.com>
2020-03-12 19:45:23 -0400

    added sql statements to controller functions as comments
    sql statements formulated from developed mysql tables in mysql file that will be pushed later

--- api/controllers/matchesController.js
+++ api/controllers/matchesController.js
@@ -12,13 +12,21 @@ const Matches = require('../models/matches');
 // ====================
 
 // load matches and serve onto index page
+//for now queries will be placed into the export code blacks for now and will be updated as proper code later
 exports.getAllMatches = (req, res, next) => {
-	
+	/*
+	SELECT * FROM matches
+	WHERE matches.userID = req.userID
+	*/
 };
 
 // get one match entry
 exports.getOneMatch = (req, res, next) => {
-
+	/*
+	SELECT * FROM matches
+	WHERE matches.userID = req.userID AND where matches.matchID = req.matchID
+	
+	*/
 };
 
 // create match
@@ -28,12 +36,20 @@ exports.createMatch = (req, res, next) => {
 
 // update Match
 exports.updateMatch = (req, res, next) => {
+	/*
+
+Insert into matches(userID,Title, teamID1,teamID2,score1,score2,description,winner)
+values(req.userID,req.title,req.teamID1,req.teamID2,req.score1,req.score2,req.description)
+
+*/
 
 };
 
 // delete Match
 exports.deleteMatch = (req, res, next) => {
-
+/*
+DELETE FROM matches WHERE matches.matchID = req.matchID
+*/
 };
 
 
