0b4fe68fbb70cceadac57dad57171251971436d9
myku5980 <40702716+myku5980@users.noreply.github.com>
2020-03-12 20:10:22 -0400

    Update usersController.js

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -20,19 +20,44 @@ const User = require('../models/user');
 // returns a list of users
 exports.getUsersList = (req, res, next) => {
 	
+	/*
+	select * FROM userList
+	*/
+
 };
 
 // signup user
 exports.signup = (req, res, next) => {
+	/*
 	
+	insert into userList(username,password,email)
+	values(req.username,req.password,req.email)
+	
+	*/
 };
 
 // login user
 exports.login = (req, res, next) => {
 	
+/*
+	
+	insert into userList(username,password,email)
+	values(req.username,req.password,req.email)
+	
+	*/
+
+
 };
 
 // delete user
 exports.delete = (req, res, next) => {
+	
+	/*
+	
+	delete from userlist
+	where userlsit.username = req.username
+	
+	*/
+	
 	
 };
