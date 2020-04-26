707b45f843d610961a3effb87dfad31920ee2f3a
myku5980 <40702716+myku5980@users.noreply.github.com>
2020-04-26 17:49:06 -0400

    Update check-auth.js

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,7 +2,7 @@
 // imports
 // ====================
 const jwt = require('jsonwebtoken');
-
+var stok = require('../../nodemon.json')
 // ====================
 // exports
 // ====================
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(" ")[1];
 		console.log(token);
-		const decoded = jwt.verify(token, process.env.JWT_KEY);
+		const decoded = jwt.verify(token, stok.env.JWT_KEY);
 		req.userData = decoded;
 		next();
 	} catch(error) {
