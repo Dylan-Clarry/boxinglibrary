cbe67de7dce7f0adec717b2dc68c9332de9b7d7a
myku5980 <40702716+myku5980@users.noreply.github.com>
2020-04-27 10:00:53 -0400

    Update db.js

--- api/middleware/db.js
+++ api/middleware/db.js
@@ -1,10 +1,9 @@
-
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'mydb'
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'bf69ec0a6976da',
+    password: '556a07c1',
+    database: 'heroku_587f81ca594c90c'
   })
   
   
