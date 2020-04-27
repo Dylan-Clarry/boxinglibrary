e5ead549ae3fff55fb8467b5f7a32acadcc07698
myku5980 <40702716+myku5980@users.noreply.github.com>
2020-04-27 10:43:48 -0400

    Update matches.js

--- api/routes/matches.js
+++ api/routes/matches.js
@@ -31,7 +31,18 @@ router.get('/matches', (req,res,next)=>{
 	});
 
 });
+//get match by matchID
+router.get('/matches/:matchID', (req,res,next)=>{
+    let sql = "SELECT matchID, userID,description,score1,score2,teamID1,teamID2,t.name as teamID1_Name,t1.name as teamID2_Name FROM matches m JOIN teams t ON t.teamID  = m.teamID1 JOIN teams t1 ON t1.teamID = m.teamID2 where matchID = ?"
+	
+	console.log(conn);
+	conn.query(sql,[req.params.matchID],(err,result)=>{
+		if(err) throw err;
+		console.log(result);
+		res.send(result)
+	});
 
+});
 // get all matches belonging to a user
 router.get('/matches/:userID', (req,res,next)=>{
     let sql = "SELECT matchID, userID,description,score1,score2,teamID1,teamID2,t.name as teamID1_Name,t1.name as teamID2_Name FROM matches m JOIN teams t ON t.teamID  = m.teamID1 JOIN teams t1 ON t1.teamID = m.teamID2 WHERE userID = ?"
